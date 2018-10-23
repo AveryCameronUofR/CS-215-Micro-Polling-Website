4923802ad837494da69498543afae06e5c9c023e
Avery <cameroav@uregina.ca>
2018-10-23 10:30:06 -0600

    Added character check to input boxes in poll creation

--- JavaScript/Poll-r.js
+++ JavaScript/Poll-r.js
@@ -1,8 +1,8 @@
 document.getElementById("createPoll").addEventListener("submit", validatePoll, false);
 document.getElementById("questionInput").addEventListener("keyup", updateQCount, false);
 var id = document.getElementById("opInput1");
-document.getElementById("option1").addEventListener("keypress", updateOpCount, false);
-document.getElementById("op2Input").addEventListener("keypress", updateOpCount, false);
-document.getElementById("op3Input").addEventListener("keyup", updateOpCount, false);
-document.getElementById("op4Input").addEventListener("keyup", updateOpCount, false);
-document.getElementById("op5Input").addEventListener("keyup", updateOpCount, false)
+document.getElementById("opInput1").addEventListener("keyup", updateOpCount, false);
+document.getElementById("opInput2").addEventListener("keyup", updateOpCount, false);
+document.getElementById("opInput3").addEventListener("keyup", updateOpCount, false);
+document.getElementById("opInput4").addEventListener("keyup", updateOpCount, false);
+document.getElementById("opInput5").addEventListener("keyup", updateOpCount, false)
--- JavaScript/charCounter.js
+++ JavaScript/charCounter.js
@@ -4,11 +4,9 @@ function updateQCount(event){
   var remaining = 150;
   if (counter <= 150){
     document.getElementById("qCounter").innerHTML = (counter + '/' + (remaining-counter));
-    event.target.className = "";
     document.getElementById("qCounter").className = ""
   } else {
     document.getElementById("qCounter").innerHTML = (counter  + ' character limit exceeded by ' + (remaining-counter)*-1);
-    event.target.className = "warning";
     document.getElementById("qCounter").className = "warning"
   }
   return false;
@@ -16,18 +14,17 @@ function updateQCount(event){
 
 function updateOpCount(event){
   var elements = event.currentTarget;
-  var text2 = elements.value;
-  var text = document.getElementById("questionInput").value;
+  var id = elements.id;                         //will equal opInput1 or whichever textbox changed
+  var targetId = "op" +id.substring(7,8)+"Count"
+  var text = elements.value;
   var counter = text.length;
-  var remaining = 150;
-  if (counter <= 150){
-    document.getElementById("qCounter").innerHTML = (counter + '/' + (remaining-counter));
+  var remaining = 50;
+  if (counter <= 50){
+    document.getElementById(targetId).innerHTML = (counter + '/' + (remaining-counter));
     event.target.className = "";
-    document.getElementById("qCounter").className = ""
   } else {
-    document.getElementById("qCounter").innerHTML = (counter  + ' character limit exceeded by ' + (remaining-counter)*-1);
-    event.target.className = "warning";
-    document.getElementById("qCounter").className = "warning"
+    document.getElementById(targetId).innerHTML = (counter  + ' character limit exceeded by ' + (remaining-counter)*-1);
+    document.getElementById(targetId).className = "warning"
   }
   return false;
 }
